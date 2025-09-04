a54c20eebf7580198b666239d6cec068e2dfd929
Yassine Sekkah <yassinesekkah11@gmail.com>
2025-09-04 12:30:22 +0100

    transfere le nombre de notes qui ajoute vers un variable

--- main.c
+++ main.c
@@ -3,58 +3,56 @@
 
 int main()
 {
-    float note[5];
-    float somme, moyenne;
+    int totnotes;
+
+    printf("combien de notes faut-il calculer: \n");
+    scanf("%d", &totnotes);
+
+    float note[totnotes];
+    float somme = 0, moyenne;
     int i;
     float max, min;
 
 
-    for(i =0; i<5; i++)
+    for(i =0; i<totnotes; i++)
     {
         printf("Entree la note %d :\n", i+1);
         scanf("%f", &note[i]);
-    }
-    somme=0;
-    for(i=0; i<5; i++)
-    {
         somme = somme + note[i];
     }
     printf("le totale des notes est %.2f\n", somme);
 
-    moyenne = somme / 5;
+    moyenne = somme / totnotes;
     printf("la moyenne des notes est %.2f\n", moyenne);
 
 
 
     max = note[0];
-    for(i = 1; i < 5; i++) {
-    if(note[i] > max) {
-        max = note[i];
-    }
+    min = note[0];
+    for(i = 1; i < totnotes; i++) {
+        if(note[i] > max) {
+            max = note[i];
+        }
+        if(note[i] < min) {
+            min = note[i];
+        }
     }
     printf("la note la plus haute est %.2f\n", max);
+    printf("la note la plus basse est %.2f\n", min);
 
-    min = note[0];
-    for(i = 1; i < 5; i++) {
-        if(note[i] < min) {
-        min = note[i];
-        }
-    }    printf("la note la plus basse est %.2f\n", min);
-
-
-
-    if (moyenne>=90) printf("Excellent");
+    if (moyenne>=90) printf("Excellent\n");
     else if(moyenne>=80 && moyenne <90)   printf("Tres bien\n");
     else if (moyenne >= 70 && moyenne <80) printf("Bien\n");
     else if (moyenne>=60 && moyenne <70 ) printf ("Passable\n");
     else if (moyenne<60) printf ("Echec\n");
 
 
-    if (somme>= 50) printf("reussi\n");
+    if (somme>= 50) {
+        printf("reussi\n");
+    }
     else{
         printf("echoue\n");
     }
-
 
     return 0;
 }
